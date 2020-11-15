831b99d1f6ec8589398f59b0b847f1a4f731e04e
choco_py3 <macaroniprfm@gmail.com>
2020-11-15 12:12:01 +0900

    2020_11_15

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import random
-from PIL import Image, ImageFilter, ImageFont
+from PIL import Image, ImageFilter, ImageFont, ImageDraw,
 app = Flask(__name__)
 
 @app.route('/')
