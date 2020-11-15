18238eb9e979db0cd1349af16fa5be3fb6592264
choco_py3 <macaroniprfm@gmail.com>
2020-11-15 12:42:40 +0900

    2020_11_15

--- app.py
+++ app.py
@@ -9,21 +9,17 @@ def hello_world():
     return render_template("index.html", numbers = numbers)
 
 @app.route('/about')
-def This_is(font_path="images/font.ttf", dst_path="dst.png", text="■", font_size=32, font_color="white"):
-    font = ImageFont.truetype(font_path, font_size)
-    # get fontsize
+def This_is():
+    testimage_path = 'images/testimage.png'
+    testimage = Image.open(testimage_path).copy()
 
-    tmp = Image.new('RGBA', (1, 1), (0, 0, 0, 0)) # dummy for get text_size
+    position = ("50", "50")
+    font = ImageFont.truetype("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap", 12px)
+    draw = ImageDraw.Draw(testimage)
 
-    tmp_d = ImageDraw.Draw(tmp)
-    text_size = tmp_d.textsize(text, font)
-    # draw text
+    draw.text(position, "■", "000000", font=font)
 
-    img = Image.new('RGBA', text_size, (0, 0, 0, 0)) # background: transparent
-
-    img_d = ImageDraw.Draw(img)
-    img_d.text((0, 0), text, fill=font_color, font=font)
-    testimage = img.save(dst_path)
+    testimage = img.save(testimage_path)
     return render_template("about.html", testimage=testimage)
 
 if __name__ == '__main__':
