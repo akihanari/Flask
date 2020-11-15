0d670a0a3dfefc70746510523a7ec033788a7bf8
choco_py3 <macaroniprfm@gmail.com>
2020-11-15 11:42:05 +0900

    2020_11_15

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 import random
+import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFont
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,8 +11,29 @@ def hello_world():
     return render_template("index.html", numbers = numbers)
 
 @app.route('/about')
-def This_is():
-    return 'This is Flask'
+    def This_is():
+        # 使うフォント，サイズ，描くテキストの設定
+    ttfontname = "C:\\Windows\\Fonts\\meiryob.ttc"
+    fontsize = 12
+    text = "■"
+
+    # 画像サイズ，背景色，フォントの色を設定
+    canvasSize    = (300, 150)
+    backgroundRGB = (255, 255, 255)
+    textRGB       = (0, 0, 0)
+
+    # 文字を描く画像の作成
+    img  = PIL.Image.new('RGB', canvasSize, backgroundRGB)
+    draw = PIL.ImageDraw.Draw(img)
+
+    # 用意した画像に文字列を描く
+    font = PIL.ImageFont.truetype(ttfontname, fontsize)
+    textWidth, textHeight = draw.textsize(text,font=font)
+    textTopLeft = (canvasSize[0]//6, canvasSize[1]//2-textHeight//2) # 前から1/6，上下中央に配置
+    draw.text(textTopLeft, text, fill=textRGB, font=font)
+
+    
+    return img.save("image.png")
 
 if __name__ == '__main__':
     app.run()
