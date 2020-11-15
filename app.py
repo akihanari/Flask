7c6b714b512d67c73ef2051f83c9c453df8eef45
choco_py3 <macaroniprfm@gmail.com>
2020-11-15 12:50:09 +0900

    2020_11_15

--- app.py
+++ app.py
@@ -9,18 +9,36 @@ def hello_world():
     return render_template("index.html", numbers = numbers)
 
 @app.route('/about')
-def This_is():
-    testimage_path = 'images/testimage.png'
-    testimage = Image.open(testimage_path).copy()
+SIZE = W, H = 28, 28
 
-    position = ("50", "50")
-    font = ImageFont.truetype("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap", 12)
-    draw = ImageDraw.Draw(testimage)
+def num_to_english(x):
+    """ 数字の英語文字列を返す """
+    assert 0 <= x <= 9, "Input int x (0 <= x <= 9)"
+    return ("ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE")[x]
 
-    draw.text(position, "■", "000000", font=font)
 
-    testimage = img.save(testimage_path)
-    return render_template("about.html", testimage=testimage)
+def make_image(idx, font_name):
+    """ フォントを指定して 0-9 の画像ファイルを作る """
+    fnt = ImageFont.truetype("C:/Windows/Fonts/{}".format(font_name), 25)
+    for i in range(10):
+        back_image = Image.new("RGBA", SIZE, (255, 255, 255, 0))
+        txt_image = Image.new('RGBA', SIZE, (0, 0, 0, 255))
+        draw = ImageDraw.Draw(txt_image)
+
+        tw, th = fnt.getsize(str(i))  # フォントを指定した時のサイズ（位置計算に使用）
+        draw.text(((W - tw) / 2, (H - th) / 2), str(i), font=fnt, fill=(255, 255, 255, 255))
+        file_name = "{}_{}.png".format(num_to_english(i), idx)
+        out = Image.alpha_composite(back_image, txt_image)
+        out.save("images/" + file_name)
+
+
+def main():
+    # ttfファイルのみ取得する
+    rp = re.compile(".*ttf")
+    font_list = [fnt for fnt in os.listdir("C:/Windows/Fonts") if rp.match(fnt)]
+
+    for idx, font_name in enumerate(font_list):
+        make_image(idx, font_name)
 
 if __name__ == '__main__':
     app.run()
