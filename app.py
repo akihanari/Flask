5f26c6791ca796a97e15680273162e6127fe92ad
choco_py3 <macaroniprfm@gmail.com>
2020-11-15 12:59:07 +0900

    2020_11_15

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ def hello_world():
     return render_template("index.html", numbers = numbers)
 
 @app.route('/about')
-SIZE = W, H = 28, 28
+W = 28
+H = 28
+
 
 def num_to_english(x):
     """ 数字の英語文字列を返す """
@@ -19,15 +21,15 @@ def num_to_english(x):
 
 def make_image(idx, font_name):
     """ フォントを指定して 0-9 の画像ファイルを作る """
-    fnt = ImageFont.truetype("C:/Windows/Fonts/{}".format(font_name), 25)
+    fnt = ImageFont.truetype("C:/Windows/Fonts/BAUHS93.TTF".format(font_name), 25)
     for i in range(10):
-        back_image = Image.new("RGBA", SIZE, (255, 255, 255, 0))
-        txt_image = Image.new('RGBA', SIZE, (0, 0, 0, 255))
+        back_image = Image.new("RGBA", (W, H), (255, 255, 255, 0))
+        txt_image = Image.new('RGBA', (W, H), (0, 0, 0, 255))
         draw = ImageDraw.Draw(txt_image)
 
         tw, th = fnt.getsize(str(i))  # フォントを指定した時のサイズ（位置計算に使用）
         draw.text(((W - tw) / 2, (H - th) / 2), str(i), font=fnt, fill=(255, 255, 255, 255))
-        file_name = "{}_{}.png".format(num_to_english(i), idx)
+        file_name = "testimage.png".format(num_to_english(i), idx)
         out = Image.alpha_composite(back_image, txt_image)
         out.save("images/" + file_name)
 
