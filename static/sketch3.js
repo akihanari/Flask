87613fe63f60dfa63e3bad50d5c62d22be9ff6ff
choco_py3 <akihanari@gmai.com>
2021-01-02 11:38:54 +0900

    20210102

--- static/sketch3.js
+++ static/sketch3.js
@@ -6,8 +6,8 @@ var shapes = [];
 
 function setup() {
   createCanvas(720, 400);
-  img = loadImage("./bg_chiheisen_green.jpg");  // Load the image
-  img2 = loadImage("./animal_alpaca_huacaya.png");
+  img = loadImage("bg_chiheisen_green.jpg");  // Load the image
+  img2 = loadImage("animal_alpaca_huacaya.png");
   shapes.push(new Shape(-r/2, height/2 + 10, true, 1));
   shapes.push(new Shape(0, height/2 + 10, false, 0));
 
